d34a30e72f28fd6ed3b63e489d24642038a98928
Gabriel Valle <gvalle91@gmail.com>
2018-06-26 18:49:44 +0000

    automatically backed up by learn

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,20 @@
 class CLI
 
   def call
+    puts "Welcome to Tic Tac Toe!"
+    game = Game.new
+    until game.won?
+      game.play
+    end
 
+
+    play_again = nil
+    until play_again.downcase == "n" || play_again.downcase == "no"
+      puts "Would you like to play again?"
+      play_again = gets.strip
+      if play_again.downcase == "y" || play_again.downcase == "yes"
+          self.play_game
+        end
+    end
   end
 end
