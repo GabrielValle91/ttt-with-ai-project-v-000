cc92678d6c594d6637cfcfe422b2e48af9ff99a7
Gabriel Valle <gvalle91@gmail.com>
2018-06-26 20:00:23 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -2,17 +2,8 @@ require_relative "../player.rb"
 class Human < Player
 
   def move(board)
-    puts "Enter a position"
+    puts "Enter a position 1-9"
     input = gets.strip
-    while !board.valid_move?(input)
-      puts "Enter a position"
-      input = gets.strip
-    end
-
-    board.update(input, self)
-
-    input
-
   end
 
 end
