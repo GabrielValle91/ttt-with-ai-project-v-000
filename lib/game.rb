ce5f5c71f38302f830778ce59edc28f0a87f76f4
Gabriel Valle <gvalle91@gmail.com>
2018-06-26 20:01:02 +0000

    automatically backed up by learn

--- lib/game.rb
+++ lib/game.rb
@@ -32,20 +32,12 @@ class Game
   end
 
   def play
-#<<<<<<< HEAD
     while !(self.draw? || self.won?)
       self.turn
-      #binding.pry
       break if (self.draw? || self.won?)
     end
-    #binding.pry
     puts "Congratulations #{self.winner}!" if self.won?
     puts "Cat's Game!" if self.draw?
-#=======
-#    while !self.draw? && !self.won?
-#      turn
-#    end
-#>>>>>>> 6b02a6b34786c5b1ba459aa2f275947c28b81866
   end
 
   def current_player
